4f48ec509ad7ee8fa34fe9b3e562d83846051d59
QuelloKun <QuelloKun@users.noreply.github.com>
2024-09-02 12:14:12 -0500

    create user, create expense, list expenses. installed react-toastify.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,18 +6,21 @@ import Expenses from './pages/Expenses';
 import Balances from './pages/Balances';
 import More from './pages/More';
 import Filler from './pages/Filler';
+import CreateUser from './pages/CreateUser';
+import ExpenseDetails from './components/ExpenseDetails';
+import AddExpense from './components/AddExpense';
 
 function App() {
-
-  
-
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
+        <Route path="createuser" element={<CreateUser />} />
         
         <Route path="manage" element={<ManageLayout />}>
           <Route path="expenses" element={<Expenses />} />
+          <Route path="expenses/:id" element={<ExpenseDetails />} />
+          <Route path="expenses/add" element={<AddExpense />} />
           <Route path="balances" element={<Balances />} />
           <Route path="more" element={<More />} />
         </Route>
@@ -30,5 +33,8 @@ function App() {
   return <RouterProvider router={router} />;
 
 }
+
+
+
 
 export default App;
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -5,7 +5,7 @@ const HomePage = () => {
   const  [users,setUsers] = useState([]);
 
   const fetchAPI = async () => {
-    const response = await axios.get("http://localhost:5000/api");
+    const response = await axios.get("https://66d5d955f5859a704267a78f.mockapi.io/api/users");
     console.log(response.data[0].name);
     setUsers(response.data[0].name);
   };
