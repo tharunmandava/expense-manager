0476bd5f679ea6f8588cc4f9b41f01674d73e450
Whizzkid <tharunmandava@gmail.com>
2024-09-03 18:34:52 +0530

    added more .sql api calls

--- server/database.sql
+++ server/database.sql
@@ -2,15 +2,56 @@
 
 CREATE TABLE users (
     user_id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(255) UNIQUE NOT NULL);
+    name VARCHAR(255) UNIQUE NOT NULL
+);
 
-#create a table expenses with expense id, paid by, amount and expense date
+#create a table expenses with expense id, paid by, amount and expense date and add a paid for field with a foreign key to users
 
 CREATE TABLE expenses (
     expense_id BIGSERIAL PRIMARY KEY,
-    paid_by VARCHAR(255) NOT NULL,
+    paid_by INTEGER NOT NULL,
     amount FLOAT NOT NULL,
-    expense_date DATE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    foreign key (paid_by) references users(name));
+    expense_date DATE DEFAULT CURRENT_TIMESTAMP,
+    group_id BIGINT DEFAULT NULL,
+    FOREIGN KEY (paid_by) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (group_id) REFERENCES groups(group_id) ON DELETE SET NULL
+);
 
+
+/* SELECT currval(pg_get_serial_sequence('expenses', 'expense_id')); 
+    currval used to retrieve the most recently entered expense_id
+*/
+
+CREATE TABLE expense_participants (
+    expense_id BIGINT NOT NULL,
+    user_id BIGINT NOT NULL,
+    PRIMARY KEY (expense_id, user_id),
+    FOREIGN KEY (expense_id) REFERENCES expenses(expense_id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+CREATE TABLE groups (
+    group_id BIGSERIAL PRIMARY KEY,
+    group_name VARCHAR(255) UNIQUE NOT NULL
+);
+
+CREATE TABLE group_members (
+    group_id BIGINT NOT NULL,
+    user_id BIGINT NOT NULL,
+    PRIMARY KEY (group_id, user_id),
+    FOREIGN KEY (group_id) REFERENCES groups(group_id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+/*
+
+SELECT ep.expense_id, u.name AS user_name FROM expense_participants ep JOIN users u ON ep.user_id = u.user_id WHERE ep.expense_id = 1;
+
+this returns a list of all the members of an expense but in the form of names instead of id
+
+
+SELECT e.expense_id, e.amount, e.expense_date FROM expenses e JOIN expense_participants ep ON e.expense_id = ep.expense_id WHERE ep.user_id = 1;
+
+SELECT expense_id, amount, expense_date FROM expenses WHERE paid_by = 1;
+
+*/
--- server/index.js
+++ server/index.js
@@ -14,8 +14,8 @@ app.use(express.json())
 
 //routes 
 
-//get list from users
-app.get("/listusers", async(req,res) =>{
+//users
+app.get("/api/list-users", async(req,res) =>{
     try {
         const data = await pool.query("SELECT * FROM users");
         res.status(200).send(data.rows);
@@ -24,41 +24,83 @@ app.get("/listusers", async(req,res) =>{
     }
 });
 
-//insert into table users
-app.post("/createuser", async(req,res) =>{
+app.get("/api/user/:id", async(req,res) =>{
+    const {id} = req.params;
     try {
-        await pool.query("INSERT INTO users(name) VALUES($1)",[req.body.name]);
-        res.status(200).send({message:"user created"});
-   
+       const data = await pool.query("SELECT * FROM users WHERE user_id = $1", [id]);
+       res.status(200).send(data.rows[0]);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+app.post("/api/create-user", async(req,res) =>{
+    try {
+        const insert = await pool.query("INSERT INTO users(name) VALUES($1) RETURNING user_id",[req.body.name]);
+        res.status(200).send({message:"User created successfully",id:insert.rows[0].user_id}); //returns the id of the user after creation
     } catch (error) {
         console.log(error);        
         res.sendStatus(500);
     }
 })
 
-//list expenses
-app.get("/listexpenses", async(req,res) =>{
+//expenses
+app.get("/api/list-expenses", async(req,res) =>{
     try {
        const data = await pool.query("SELECT * FROM expenses"); 
        res.status(200).send(data.rows);
     } catch (error) {
        console.log("error getting list-expenses",error); 
     }
-})
+});
 
-app.post("/createexpense", async(req,res) =>{
+//list expenses paid by user
+app.get("/api/list-expenses-by-user/:id", async(req,res) =>{
+    const {id} = req.params;
     try {
-        
+        const data = await pool.query("SELECT expense_id, amount, expense_date FROM expenses WHERE paid_by = $1", [id]);
+        res.status(200).send(data.rows);
     } catch (error) {
         
     }
-})
+});
+
+
+//detailed info about an expense
+app.get("/api/expense/:id", async(req,res) =>{
+    const {id} = req.params;
+    try {
+        const data = await pool.query("SELECT * FROM expenses WHERE expense_id = $1", [id]);
+        const data2 = await pool.query("SELECT ep.expense_id, u.name AS user_name FROM expense_participants ep JOIN users u ON ep.user_id = u.user_id WHERE ep.expense_id = $1;", [id]);
+        const user_names = data2.rows.map(row => row.user_name);
+        const obj = {
+             transaction: data.rows[0], 
+             participants: user_names     // Array of users
+        };
+        res.status(200).send(obj);   
+        
+    } catch (error) {
+       res.status(500).send(error);
+    }
     
+});
+
+app.post("/api/create-expense", async(req,res) =>{
+    const { paid_by, amount } = req.body;
+    try {
+        const result = await pool.query("INSERT INTO expenses(paid_by,amount) VALUES($1, $2) RETURNING expense_id", [paid_by, amount]);
+        res.status(200).send({message:"Expense created successfully",id:result.rows[0].expense_id});
+    } catch (error) {
+       console.log(error);
+       res.sendStatus(500); 
+    }
+})
 
 
 app.listen(5000,() => {
     console.log("server started!");
 });
+
 
 
 
