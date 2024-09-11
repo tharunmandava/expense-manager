93bb162344a4213166ad61e85ce12190bd1e448a
Whizzkid <tharunmandava@gmail.com>
2024-09-11 17:27:28 +0530

    added ui library

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Button} from 'antd';
 
 const HomePage = () => {
   const handleClearLocalStorage = () => {
@@ -7,15 +8,26 @@ const HomePage = () => {
   };
 
   return (
-    <div className="p-4">
-      <button
-        onClick={handleClearLocalStorage}
-        className="bg-red-500 text-white font-semibold py-2 px-4 rounded hover:bg-red-600 transition-colors"
-      >
-        Clear Local Storage
-      </button>
+    <>
+     
+          <div className="min-h-screen">
+      <div className="h-[333px] bg-red-500 flex justify-center items-center">
+        <h1 className='text-5xl text-white'>Share Expenses with your friends and family</h1>
+          <Button type='primary'
+            onClick={handleClearLocalStorage}
+            className="bg-red-500 text-white font-semibold py-2 px-4 rounded hover:bg-red-600 transition-colors">
+            Clear Local Storage
+          </Button>
+      </div>
+      <div className="h-[333px] bg-green-500 flex justify-center items-center">
+        <h1 className="text-white text-3xl">Section 2</h1>
+      </div>
+      <div className="h-[334px] bg-blue-500 flex justify-center items-center">
+        <h1 className="text-white text-3xl">Section 3</h1>
+      </div>
     </div>
-  );
+  </>
+);
 };
 
 export default HomePage;
